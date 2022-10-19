52a6959f85c0c6cd82eb1b48fdb7aca54aed3cc8
Hidenori Murai <hidenoweb822@gmail.com>
2022-10-19 22:03:22 +0900

    トップスクロール追加

--- js/script.js
+++ js/script.js
@@ -59,3 +59,37 @@ $(function () {
 $(function () {
   $("html,body").animate({ scrollTop: 0 }, "1");
 });
+
+//TOP誘導ボタン
+jQuery(function () {
+  var appear = false;
+  var pagetop = $("#c-page-top");
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 350) {
+      //100pxスクロールしたら
+      if (appear == false) {
+        appear = true;
+        pagetop.stop().animate(
+          {
+            bottom: "50px", //下から50pxの位置に
+          },
+          300
+        ); //0.3秒かけて現れる
+      }
+    } else {
+      if (appear) {
+        appear = false;
+        pagetop.stop().animate(
+          {
+            bottom: "-50px", //下から-50pxの位置に
+          },
+          300
+        ); //0.3秒かけて隠れる
+      }
+    }
+  });
+  pagetop.click(function () {
+    $("body, html").animate({ scrollTop: 0 }, 500); //0.5秒かけてトップへ戻る
+    return false;
+  });
+});
